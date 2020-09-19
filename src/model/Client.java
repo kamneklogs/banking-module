fdc49238f7a778fb1f6886b739a6d76f447adfb4
kamneklogs <kamneklogs@gmail.com>
2020-09-19 16:51:06 -0500

    added  Client and User classes

---
+++ src/model/Client.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.Date;
+
+public class Client extends User {
+
+    private double balance;
+    private double creditQuota, limit;
+
+    // Pago de tarjeta de credito y fecha de registro
+    private Date datePayC, registrationDate;
+
+    // Politica de banco: para limite credito sera balance*0.1
+    public Client(String name, String id, double balance, double creditQuota, Date datePayC, Date registrationDate) {
+        super(name, id);
+        this.balance = balance;
+        this.creditQuota = creditQuota;
+        this.datePayC = datePayC;
+        this.registrationDate = registrationDate;
+        limit = balance * 0.1;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getCreditQuota() {
+        return limit - creditQuota;
+    }
+
+    public Date getDatePayC() {
+        return datePayC;
+    }
+
+    public void setDatePayC(Date datePayC) {
+        this.datePayC = datePayC;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+}
---
+++ src/model/User.java
@@ -0,0 +1,27 @@
+package model;
+
+public class User {
+    private String name, id;
+
+    public User(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
