41046d551d1ebaf5b3fb33fea86ea1ebf1fbf0a8
andreanr19 <47890681+andreanr19@users.noreply.github.com>
2020-09-20 17:51:06 -0500

    Added some fxml

--- src/ui/BankController.java
+++ src/ui/BankController.java
@@ -9,34 +9,94 @@ import javafx.scene.Parent;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import model.QueueModule.QueueModule;
+import model.cashierModule.CashierModule;
 
 public class BankController {
 
 	private QueueModule queueModule;
 
+	private CashierModule cashierModule;
+
 	@FXML
-	private ToggleGroup modulos;
+	private ToggleGroup opsCashiersModule;
+
+	@FXML
+	private ToggleGroup opsQueueModule;
 
 	@FXML
 	private Pane mainPane;
 
 	@FXML
-	void loadCashierModule(ActionEvent event) throws IOException {
-
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cashierModule.fxml"));
+	void createAClientAccount(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createClientAccount.fxml"));
 		fxmlLoader.setController(this);
-		Parent cashierm = fxmlLoader.load();
+		Parent clientAccount = fxmlLoader.load();
 		mainPane.getChildren().clear();
-		mainPane.getChildren().add(cashierm);
+		mainPane.getChildren().add(clientAccount);
 	}
 
 	@FXML
-	void loadQueueModule(ActionEvent event) throws IOException {
-
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("queueModule.fxml"));
+	void deleteClientAccount(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("deleteClientAccount.fxml"));
 		fxmlLoader.setController(this);
-		Parent queueModule = fxmlLoader.load();
+		Parent clientAccount = fxmlLoader.load();
 		mainPane.getChildren().clear();
-		mainPane.getChildren().add(queueModule);
+		mainPane.getChildren().add(clientAccount);
+	}
+
+	@FXML
+	void doPayOption(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("doPayOption.fxml"));
+		fxmlLoader.setController(this);
+		Parent clientAccount = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(clientAccount);
+	}
+
+	@FXML
+	void payCreditCard(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("payCreditCard.fxml"));
+		fxmlLoader.setController(this);
+		Parent clientAccount = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(clientAccount);
+	}
+
+	@FXML
+	void searchAClient(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("searchAClient.fxml"));
+		fxmlLoader.setController(this);
+		Parent clientAccount = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(clientAccount);
+	}
+
+	@FXML
+	void seeClientsDataBase(ActionEvent event) throws IOException {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("seeClientsDataBase.fxml"));
+		fxmlLoader.setController(this);
+		Parent clientAccount = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(clientAccount);
+	}
+
+	@FXML
+	void seeQueuesState(ActionEvent event) throws IOException {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("seeQueuesState.fxml"));
+		fxmlLoader.setController(this);
+		Parent clientAccount = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(clientAccount);
+	}
+
+	@FXML
+	void assignTurn(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("assignTurn.fxml"));
+		fxmlLoader.setController(this);
+		Parent clientAccount = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.getChildren().add(clientAccount);
 	}
 }
