b6df428120a0f30f08f8f71239d5302a9d6e0d2a
andreanr19 <47890681+andreanr19@users.noreply.github.com>
2020-09-30 18:20:44 -0500

    Added the HeapSort tests methods

--- src/model/CashierModule/HeapSortLibrary/HeapSort.java
+++ src/model/CashierModule/HeapSortLibrary/HeapSort.java
@@ -4,54 +4,56 @@ import model.CashierModule.HeapSortLibrary.GenericArray.GenericArray;
 
 public class HeapSort<V extends Comparable<V>> {
 
-    private int n;
+	private int n;
 
-    private GenericArray<V> theArray;
+	private GenericArray<V> theArray;
 
-    private void sort() {
+	private void sort() {
+		if (n == 0) {
 
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(theArray, n, i);
-        }
+		} else {
+			for (int i = n / 2 - 1; i >= 0; i--) {
+				heapify(theArray, n, i);
+			}
 
-        for (int i = n - 1; i > 0; i--) {
-            V temp = theArray.get(0);
-            theArray.set(0, theArray.get(i));
-            theArray.set(i, temp);
+			for (int i = n - 1; i > 0; i--) {
+				V temp = theArray.get(0);
+				theArray.set(0, theArray.get(i));
+				theArray.set(i, temp);
 
-            heapify(theArray, i, 0);
-        }
+				heapify(theArray, i, 0);
+			}
+		}
+	}
 
-    }
+	private void heapify(GenericArray<V> a, int n, int i) {
+		int largest = i;
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
 
-    private void heapify(GenericArray<V> a, int n, int i) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+		if (left < n && a.get(left).compareTo(a.get(largest)) < 0) {
+			largest = left;
+		}
 
-        if (left < n && a.get(left).compareTo(a.get(largest)) < 0) {
-            largest = left;
-        }
+		if (right < n && a.get(right).compareTo(a.get(largest)) < 0) {
+			largest = right;
+		}
 
-        if (right < n && a.get(right).compareTo(a.get(largest)) < 0) {
-            largest = right;
-        }
+		if (largest != i) {
+			V swap = a.get(i);
+			a.set(i, a.get(largest));
+			a.set(largest, swap);
 
-        if (largest != i) {
-            V swap = a.get(i);
-            a.set(i, a.get(largest));
-            a.set(largest, swap);
+			heapify(a, n, largest);
+		}
 
-            heapify(a, n, largest);
-        }
+	}
 
-    }
-
-    public final GenericArray<V> sortArray(GenericArray<V> theArray) {
-        this.theArray = theArray;
-        n = theArray.length;
-        sort();
-        return theArray;
-    }
+	public final GenericArray<V> sortArray(GenericArray<V> theArray) {
+		this.theArray = theArray;
+		n = theArray.length;
+		sort();
+		return theArray;
+	}
 
 }
--- tests/HeapSort/HeapSortTest.java
+++ tests/HeapSort/HeapSortTest.java
@@ -12,22 +12,72 @@ class HeapSortTest {
 
 	HeapSort<Client> myHeapSort;
 	GenericArray<Client> myGenericArray;
-	
+
 	public void setUp1() {
-		myHeapSort= new HeapSort<Client>();
-		myGenericArray= new GenericArray<Client>(3);
-		
-		myGenericArray.set(0, new Client("Andrea", 100, true, 100, 0, "29/9/2020", 0) );
+		myHeapSort = new HeapSort<Client>();
+		myGenericArray = new GenericArray<Client>(3);
+
+		myGenericArray.set(0, new Client("Andrea", 100, true, 100, 0, "29/9/2020", 0));
 		myGenericArray.set(1, new Client("Danna", 55, true, 100, 0, "29/9/2020", 0));
 		myGenericArray.set(2, new Client("Camilo", 200, true, 100, 0, "29/9/2020", 0));
 
 	}
 
+	public void setUp2() {
+		myGenericArray = new GenericArray<Client>(0);
+		myHeapSort = new HeapSort<Client>();
+
+	}
+
+	public void setUp3() {
+		myGenericArray = new GenericArray<Client>(9);
+		myHeapSort = new HeapSort<Client>();
+
+
+		myGenericArray.set(0, new Client("Andrea", 9, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(1, new Client("Danna", 8, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(2, new Client("Camilo", 7, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(3, new Client("Cristhian", 6, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(4, new Client("Pinina", 5, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(5, new Client("Niña", 4, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(6, new Client("Snow", 3, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(7, new Client("Max", 2, true, 100, 0, "29/9/2020", 0));
+		myGenericArray.set(8, new Client("Lucy", 1, true, 100, 0, "29/9/2020", 0));
+	}
+
 	@Test
-	public void sortTest() {
+	public void sortTest1() {
 		setUp1();
-		myGenericArray= myHeapSort.sortArray(myGenericArray);
-		
+		myGenericArray = myHeapSort.sortArray(myGenericArray);
+
 		assertEquals("Danna", myGenericArray.get(0).getName());
+	}
+
+	@Test
+	public void sortTest2() {
+		setUp2();
+		GenericArray<Client> myGenericArray2 = myHeapSort.sortArray(myGenericArray);
+
+		assertEquals(myGenericArray, myGenericArray2);
+
+	}
+	
+	@Test
+	public void sortTest3() {
+		setUp3();
+		
+		myGenericArray = myHeapSort.sortArray(myGenericArray);
+
+		assertEquals("Lucy", myGenericArray.get(0).getName());
+
+	}
+	@Test
+	public void sortTest4() {
+		setUp3();
+		
+		myGenericArray = myHeapSort.sortArray(myGenericArray);
+
+		assertEquals("Andrea", myGenericArray.get(8).getName());
+
 	}
 }
